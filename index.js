3cf135da4158cf48c58f1b873bcd1bf8d9fdceb1
Cole Perry <colecperry@gmail.com>
2023-02-23 20:20:04 -0800

    Done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+var bestCustomer
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Mike'
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'Ike'
+}
+
+
